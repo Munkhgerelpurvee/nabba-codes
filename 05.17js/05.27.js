f02386d9e6ce8f6ac8e3377b0f028507cc15101c
24LP8045 24LP8045 <24LP8045@G301-CC0209.local>
2024-05-27 12:34:28 +0800

    asdasd

---
+++ 05.17js/05.27.js
@@ -0,0 +1,75 @@
+// Array methods
+
+// const x = [5, 100, 7];
+// const y = x.push(1);
+// console.log(x)
+
+// const x = [5, 100, 7];
+// const y = x.pop();
+// console.log(x)
+
+// loop dawtalt n too ogohod nemegddeg function vvsgeh
+
+// function count(n) {
+//     for (let a = 1; a <= n; a++) {
+//         console.log(a)
+//     }
+// }
+// count(5)
+
+// loop dawtalt n too ogohod nemegddeg array vvsgeh
+
+// function count(n) {
+//     const arr = [];
+//     for (let a = 1; a <= n; a++) {
+//         arr.push(a);
+//     }
+//     return arr;
+// }
+// const h = count(5)
+// console.log(h)
+
+// loop dawtalt n too ogohod hasagddag array vvsgeh
+
+// function count(n) {
+//     const arr = [];
+//     for (let a = n; a >= 0; a -= 1) {
+//         arr.push(a);
+//     }
+//     return arr;
+// }
+// const h = count(5)
+// console.log(h)
+
+// loop ogogdson arreyg jagsaalt bolgon tvvnii urtaar utga bolgon hewlej ogoh
+// const a = [1, 5, 2, 7, 45];
+// const b = [ 1,2,4]
+// function printArr(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         console.log(arr[i]);
+//     }
+// }
+// printArr(a);
+// printArr(b);
+
+// arrey dotorh utguudaas heden tegsh too baigaag hewlelee
+
+// const a = [1, 4, 2, 7, 45];
+// function even(arr) {
+//     let count = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 == 0) {
+//             count++;
+//         }
+//     }
+//     return count
+// }
+// console.log(even(a));
+
+// gertei hiine
+const a = [1, 5, 6, 7, 67869, 10];
+
+function max(arr) { }
+
+const result = max(a);
+console.log(result); //67869
--- 05.17js/jsjs.js
+++ 05.17js/jsjs.js
@@ -46,18 +46,19 @@
 // }
 // const x = nasshalgah(19)
 
-function angioloh(age, gender) {
-    if ((age >= 6 && age <= 17) && gender == "er") {
-        return console.log(`${age - 5}-r angiin suragch eregtei suragch `)
-    } else if ((age >= 6 && age <= 17) && gender == "em") {
-        return console.log(`${age - 5}-r angiin emegtei suragch `)
-    } else if (age < 6) {
-        console.log(`Tsetserlegiin maamaa min surguuld orohod ${6 - age} jil vldsen baina.`)
-    } else if (age > 17) {
-        console.log(`Ta ulirsan eswel surguuld hotsorch oroogvi l bol arwan jilee togsood ${age - 17} jil bolj baina`)
-    }
-}
-const x = angioloh(15, "er")
+// suragchiin nasaar angi oloh
+// function angioloh(age, gender) {
+//     if ((age >= 6 && age <= 17) && gender == "er") {
+//         return console.log(`${age - 5}-r angiin suragch eregtei suragch `)
+//     } else if ((age >= 6 && age <= 17) && gender == "em") {
+//         return console.log(`${age - 5}-r angiin emegtei suragch `)
+//     } else if (age < 6) {
+//         console.log(`Tsetserlegiin maamaa min surguuld orohod ${6 - age} jil vldsen baina.`)
+//     } else if (age > 17) {
+//         console.log(`Ta ulirsan eswel surguuld hotsorch oroogvi l bol arwan jilee togsood ${age - 17} jil bolj baina`)
+//     }
+// }
+// const x = angioloh(15, "er")
 
 // // -Өгөгдсөн тоог шалгаад тэгш тоо бол "even" , сондгой тоо бол "odd" гэж хэвлэнэ үү .
 
@@ -118,3 +119,91 @@ const x = angioloh(15, "er")
 // // Өгөгдсөн 2 тооноос ихийг нь олж хэвлэнэ үү .
 // const x = [1, 12, 561111, 8, 15, 25, 21412]
 // console.log(Math.max(...x))
+
+// Exercises (condition && operation)
+
+// 2.Өгөгдсөн тоо нь 5-д хуваагддаг үгүйг шалгана уу.
+
+// function oloh(a) {
+//     if (a % 5 == 0) {
+//         return "5-d huwaagddag"
+//     } else {
+//         return "5-d huwaagdadgvi"
+//     }
+// }
+// const x = oloh(10)
+// console.log(x)
+
+// 3.Өгөгдсөн үсгийг шалгаад эгшиг бол "vowel" , гийгүүлэгч бол "consonant" гэж хэвлэнэ үү .
+// function oloh(vseg) {
+//     if (vseg == "a" || vseg == "e" || vseg == "i" || vseg == "o" || vseg == "u") {
+//         return "vowel"
+//     } else {
+//         return "consonant"
+//     }
+// }
+// const x = oloh("t")
+// console.log(x)
+
+// 4.Өгөгдсөн тоо 1-ээс 100-н хооронд байвал "include" , байхгүй бол "exclude" гэж хэвлэнэ үү .
+
+// function oloh(a) {
+//     if (a > 1 && a < 100) {
+//         return "include"
+//     } else {
+//         return "exclude"
+//     }
+// }
+// const x = oloh(170)
+// console.log(x)
+
+// 5. 3 тооноос хамгийн ихийг нь олж хэвлэнэ үү .
+// function max(a, b, c) {
+//     if (a > b) {
+//         if (a > c) {
+//             return a
+//         } else {
+//             return c
+//         }
+//     } else {
+//         if (b > c) {
+//             return b
+//         } else {
+//             return c
+//         }
+//     }
+// }
+// const x = max(5, 100, 7)
+// console.log(x)
+
+// 5.3 тооноос хамгийн ихийг нь олж хэвлэнэ үү .
+
+// const x = [1, 12, 561111, 8, 15, 25, 21412]
+// console.log(Math.max(...x))
+
+// 6.Өгөгдсөн жил нь Олимпийн жил мөн бишийг шалгаад хэвлэнэ үү .
+
+// function oloh(a) {
+//     if (a == 2021 || a % 4 == 0 && a >= 1896 && a !== 2020) {
+//         return "Олимпийн жил мөн"
+//     } else {
+//         return "Олимпийн жил биш"
+//     }
+// }
+// const x = oloh(2020)
+// console.log(x)
+
+// 7.Check the given number is not a prime number .
+// 8.Өгөгдсөн тоог анхны тоо мөн бишийг шалгана уу . (1 болон өөртөө хуваагддаг тоог анхны тоо гэнэ)
+// example :
+// number = 2 // true
+// number = 15 // false
+
+function oloh(a) {
+    if (a < 2) {
+        return "false"
+    }
+
+}
+const x = oloh(2020)
+console.log(x)
