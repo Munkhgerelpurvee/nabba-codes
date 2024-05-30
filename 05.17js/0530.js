6c06c73c206b67b9d69d2ca0e1eda049276042be
Naba <myagmartsognnaranbaatar@gmail.com>
2024-05-30 23:35:16 +0800

    asdasda

--- 05.17js/0530.js
+++ 05.17js/0530.js
@@ -13,19 +13,62 @@
 
 // 2. Өгөгдсөн массивын элементүүдийг хойш нь нэг цикл шилжилт хий. Өөрөөр хэлбэл 1-рийг 2-рт, 2-рыг 3-рт, гэх мэт сүүлийн элементийг 1-рт тус тус шилжүүл.
 //     Жич: 1 2 3 4 5 -> 5 1 2 3 4
-const a = [1, 2, 3, 4, 5];
-function rearrange(num) {
-    let d = 1;
-    for (let i = a.length; i < 0; i++) {
-        let temp = num[d];
-        num[d] = num[i]
-        num[i] = temp;
-        d++;
-    }
-    return num;
-}
+// const a = [1, 2, 3, 4, 5];
+// function rerange(num) {
+//     temp=num[4]
+//     num[4]=num[3]
+//     num[3]=num[2]
+//     num[2]=num[1]
+//     num[1]=num[0]
+//     num[0] = temp;
+//     return num
 
-const result = rearrange(a);
-console.log(result);
+// }
+// const result = rerange(a);
+// console.log(result); 
+
+// const a = [1, 2, 3, 4, 5];
+// function rerange(num) {
+//     temp=num[num.length - 1]
+//     for (let i = num.length - 1; i > 0; i--) {
+//         num[i] = num[i - 1];           
+//     }
+//     num[0] = temp;                    
+//     return num;
+// }
+
+// const result = rerange(a);
+// console.log(result); 
 
 // 3. Өгөгдсөн массив өсөх эрэмбээр байвал true үгүй бол false буцаа
+// const b = [20, 25, 30, 35, 40];
+// function max(toonuud) {
+//     for (let i = 1; i < toonuud.length; i++) {
+//         if (toonuud[i] < toonuud[i - 1])  {
+//             return false
+//         }  
+//     } return true
+// }
+// const result = max(b);
+// console.log(result);
+
+// 4
+const a = [84, 28, 57, 38, 37];
+function dvnoloh(a) {
+    const roundedGrades = [];
+    for (let i = 0; i < a.length; i++) {
+        const grade = a[i]; 
+        if (grade < 38) {
+            roundedGrades.push(grade);
+        } 
+        if (Math.ceil(grade / 5) * 5 - grade < 3 && grade >= 38) {
+            roundedGrades.push(Math.ceil(grade / 5) * 5);
+        } else if(grade >= 38) {
+            roundedGrades.push(grade);
+        }
+        
+    }
+    return roundedGrades;
+}
+const result = dvnoloh(a);
+console.log(result);
