d338609c3243983813835ece0c97e2d9afeea33a
24LP8045 24LP8045 <24LP8045@G301-CC0209.local>
2024-06-07 12:56:10 +0800

    asdasdafafaf

--- dom/picture.js
+++ dom/picture.js
@@ -1,8 +1,13 @@
-const imgs = document.querySelector(".img-container")
+const imgs = document.querySelectorAll("img")
 const btn1 = document.querySelector(".btn1")
+imgs.style.transform = "translatex(0px)";
+
+console.log(imgs)
+
+
 btn1.addEventListener("click", () => {
     imgs.style.border = "5px solid green";
-    imgs.style.transform = "translatex(-400px)";
+    // imgs.style.transform = "translatex(-400px)";
 })
 const btn2 = document.querySelector(".btn2")
 btn2.addEventListener("click", () => {
