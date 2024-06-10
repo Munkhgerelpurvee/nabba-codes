02fd9c744dc3c9c9148209fb55fe7d48c179d89a
24LP8045 24LP8045 <24LP8045@G301-CC0209.local>
2024-06-10 13:04:08 +0800

    asdasdasdagasa

--- dom/timer.js
+++ dom/timer.js
@@ -3,20 +3,20 @@ const startbtn = document.querySelector('.startbtn');
 const stopbtn = document.querySelector('.stopbtn');
 const resetbtn = document.querySelector('.resetbtn');
 
-let count = 0.000;
+let count = 0.0;
 let id;
 
 startbtn.addEventListener('click', () => {
     id = setInterval(() => {
-        count += 0.001;
-        timer.textContent = count.toFixed(3);
-    }, 1)
+        count += 0.1;
+        timer.textContent = count.toFixed(2);
+    }, 100)
 });
 stopbtn.addEventListener('click', () => {
     clearInterval(id);
 });
 resetbtn.addEventListener('click', () => {
     clearInterval(id);
-    timer.textContent = '0.000';
+    timer.textContent = '0.0';
     count = 0;
 });
