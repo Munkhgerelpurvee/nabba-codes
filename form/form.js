b4d678ceb7f3bf59c8bdcb4cabedc2290068e78f
24LP8045 24LP8045 <24LP8045@G301-CC0209.local>
2024-06-11 12:33:23 +0800

    akjsbfalfkla

---
+++ form/form.js
@@ -0,0 +1,15 @@
+const signupbtn = document.querySelector('#signup');
+const signinbtn = document.querySelector('#signin');
+const container1 = document.querySelector('.container');
+const container2 = document.querySelector('.container2');
+
+signupbtn.addEventListener('click', () => {
+    container1.classList.add("zindex1")
+    container2.classList.add("zindex2")
+});
+
+signinbtn.addEventListener('click', () => {
+    container1.classList.add("zindex2")
+    container2.classList.remove("zindex2")
+    container2.classList.add("zindex1")
+});
