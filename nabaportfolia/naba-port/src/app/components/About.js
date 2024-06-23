62059e0e4a70bd7b4efbd2fbfbe9ff0a9fbacc62
Naba <myagmartsognnaranbaatar@gmail.com>
2024-06-23 23:40:28 +0800

    asdasfasffsaasd

--- nabaportfolia/naba-port/src/app/components/About.js
+++ nabaportfolia/naba-port/src/app/components/About.js
@@ -1,8 +1,8 @@
 export const About = () => {
     return (
-        <div className="w-[90%] m-auto flex flex-col gap-[48px] items-center h-max rounded-[20px] py-[16px]">
-            <div className="px-[24px] py-[8px] rounded-[16px] bg-gray-500 text-white">
-                Миний тухай
+        <div className="w-[90%] m-auto flex flex-col gap-[48px]  h-max rounded-[20px] py-[16px]">
+            <div className=" flex items-center px-[24px] py-[8px] rounded-[16px] bg-gray-500 text-white">
+                <div className="flex items-center">Миний тухай</div>
             </div>
 
             <div className="flex ">
