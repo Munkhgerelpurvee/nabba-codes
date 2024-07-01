98332c1ca0c853820fb941962a65b04a40321e7e
Naba0521 <myagmartsognnaranbaatar@gmail.com>
2024-07-01 23:42:32 +0530

    asdhiaushd

--- blog/blog/src/app/component/Header.js
+++ blog/blog/src/app/component/Header.js
@@ -3,21 +3,21 @@ import { SearchIcon } from "../assets/searchicon"
 
 export const Header=()=>{
     return(
-        <div className="flex justify-between">
-            <div className="flex">
-                <div><Logo/></div>
-                <div className="flex">
-                    <div>Meta</div>
-                    <div>Blog</div>
+        <div className="flex w-[90%] items-center justify-between m-auto pt-[32px] pb-[32px]">
+            <div className="flex gap-[8px]">
+                <div className="w-[36px] h-[36px]"><Logo/></div>
+                <div className="flex items-center">
+                    <div className="text-[#141624] text-[24px]">Meta</div>
+                    <div className="text-[#141624] text-[24px] font-bold">Blog</div>
                 </div>
             </div>
-            <div className="flex">
+            <div className="flex gap-[40px] text-[16px] text-[#3B3C4A] font-medium items-center">
                 <div>Home</div>
                 <div>Blog</div>
                 <div>Contact</div>
             </div>
-            <div className="flex">
-                <input type="text" placeholder="Search"></input>
+            <div className="flex items-center w-[166px] h-[36px] bg-[#F4F4F5] pt-[8px] pr-[8px] pb-[8px] pl-[16px]">
+                <input type="text" placeholder="Search" className="w-[114px] bg-[#F4F4F5] text-[#A1A1AA] text-[14px]"></input>
                 <div className="w-[16px] h-[16px]"><SearchIcon/></div>
             </div>
         </div>
--- blog/blog/src/app/page.js
+++ blog/blog/src/app/page.js
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import { Footer } from "./component/Footer";
 import { Header } from "./component/Header";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center">
-      <div><Header/></div>
+    <main className="flex flex-col m-auto bg-white w-[1220px]">
+      <Header/>
+      <Footer/>
+      
       
     </main>
   );
