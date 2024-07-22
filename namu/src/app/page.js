2c2af5651af8d37635c0219e2c863477841263cd
24LP8045 24LP8045 <24LP8045@G301-CC0209.local>
2024-07-22 12:35:43 +0800

    asjdnkjsndkajnsdkajsd

--- namu/src/app/page.js
+++ namu/src/app/page.js
@@ -1,3 +1,29 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <main className=""></main>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch(`http://localhost:3001/`);
+        const data = await res.json();
+        setData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
+  return (
+    <main>
+      <div>asdasdasdasd</div>
+      {data.map((item) => (
+        <div key={item.title}>
+          <h1>{item.title}</h1>
+        </div>
+      ))}
+    </main>
+  );
 }
