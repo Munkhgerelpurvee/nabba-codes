101e08f2a87e3978ea076198ba65278d94bc376d
24LP8045 24LP8045 <24LP8045@G301-CC0209.local>
2024-08-22 20:29:39 +0800

    askjdnakjsfbnkaj

---
+++ darmaa/darmaa/src/app/hutulbur/page.js
@@ -0,0 +1,70 @@
+"use client";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
+import Link from "next/link";
+
+const Hutulbur = () => {
+  return (
+    <main className="bg-[#202C6A]">
+      <div className="w-[90%] m-auto flex flex-col   items-center h-max py-[40px] gap-[24px]">
+        <div className="flex justify-between w-[80%]">
+          <div className="text-white bg-[#080062] w-[126px] h-[34px] rounded-lg items-center flex justify-center border  border-[#007EF2]">
+            Танилцуулга
+          </div>
+          <div className="text-white bg-[#080062] w-[126px] h-[34px] rounded-lg items-center flex justify-center border  border-[#007EF2]">
+            Хөтөлбөр
+          </div>
+        </div>
+        <div className="flex flex-col items-start justify-start">
+          <h1 className="flex w-full justify-center text-center text-2xl font-extrabold text-[#FFCF54] dark:text-[#F9FAFB]">
+            Монгол Улсын Начин
+          </h1>
+          <h1 className="text-2xl w-full justify-center text-center text-white font-extrabold dark:text-[#F9FAFB]">
+            Галбадрахын Дармаажанцан
+          </h1>
+        </div>
+        <div className="flex flex-col lg:flex-row lg:flex gap-8">
+          <div className="flex flex-1 items-center justify-center">
+            <img className="w-full rounded-[20px]" src="/me.png" />
+          </div>
+          <div className="flex-1 flex flex-col gap-[48px]">
+            <div className="flex flex-col gap-[8px] text-white">
+              <div>Төрсөн өдөр: 2000 он 03 сар 29 өдөр </div>
+              <div>
+                Харьяалал: "Ховд" дэвжээ, "Алдар" спорт хороо, ЗХ-034 анги,
+                Бүрэн Минж Транс ХХК, УСУГ
+              </div>
+              <div className="text-white dark:text-[#F9FAFB] text-justify">
+                {" "}
+                Ховд аймгийн Дөргөн суманд төрж өссөн.
+              </div>
+              <div>
+                2020 онд Ховд аймгийн баяр наадамд зургаа давж, аймгийн арслан
+                Ц.Төмөрцоожид өвдөг шороодон үзүүрлэж " Аймгийн заан " цол
+                хүртсэн.
+              </div>
+              <div>
+                2022 онд Ховд аймгийн баяр наадамд аймгийн арслан
+                Б.Даваадалайгаар долоо даван түрүүлж " Аймгийн арслан " цол
+                хүртсэн.
+              </div>
+              <div>
+                2024 онд АХ-ын 103 жилийн ойн баяр наадамд улсын харцага
+                Г.Бадрахаар дөрөв, цэргийн арслан Г.Жамбалдоржоор тав давж "
+                Монгол улсын начин " цолыг хүртсэн.
+              </div>
+            </div>
+
+            <div className="flex gap-[40px] items-center justify-center">
+              <FaFacebookSquare className="w-[24px] h-[24px] dark:text-[#F9FAFB] text-[#F9FAFB]" />
+              <FaInstagram className="w-[24px] h-[24px] dark:text-[#F9FAFB] text-[#F9FAFB]" />
+              <BiLogoGmail className="w-[24px] h-[24px] dark:text-[#F9FAFB] text-[#F9FAFB]" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Hutulbur;
